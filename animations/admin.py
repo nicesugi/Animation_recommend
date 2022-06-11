8002306748255ac59df67e4d388e43bc17c6a780
nicesugi <104303285+nicesugi@users.noreply.github.com>
2022-06-11 20:15:36 +0900

    Add fixtures.json & Feat animations app: models,admin

--- animations/admin.py
+++ animations/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Animation, Genre
 # Register your models here.
+
+admin.site.register(Animation)
+admin.site.register(Genre)
--- animations/models.py
+++ animations/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class Genre(models.Model):
+    class Meta:
+        db_table = "genre"
+    name = models.CharField(max_length=70, default='')
+    def __str__(self):
+        return f'{self.name}'
+
+
+
+class Animation(models.Model):
+    class Meta:
+        db_table = "animation"
+    title = models.CharField(max_length=70, default='')
+    original_title = models.CharField(max_length=70, default='')
+    genre = models.ManyToManyField(Genre, related_name='animations')
+    company = models.CharField(max_length=70, default='')
+    rated = models.CharField(max_length=70, default='')
+    broadcasted_date = models.CharField(max_length=70, default='')
+    chapters = models.CharField(max_length=70, default='')
+    story = models.TextField(max_length=256, default='')
+    img = models.TextField(max_length=256, default='')
+    def __str__(self):
+        return f'{self.title}'
+
